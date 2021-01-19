ea554f4305c045a50ad920d901e81d566c04acec
Brandon Martel <brandonmartel@gmail.com>
2021-01-18 22:03:36 -0600

    removing unused method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   include Pundit
 
-  before_action :configure_permitted_parameters, if: :devise_controller?
   after_action :verify_authorized, except: :index, unless: :devise_controller?
   after_action :verify_policy_scoped, only: :index, unless: :devise_controller?
 
